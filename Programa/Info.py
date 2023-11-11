38368f3b60f626a20668f329ad6d6c64a2603110
ZhouZKZ <roberto16092004@gmail.com>
2023-11-10 22:19:21 -0300

    Códigos Úteis do Módulo

--- Programa/Info.py
+++ Programa/Info.py
@@ -23,6 +23,9 @@ driver.get("https://google.com")
 print(driver.title)"""
 
 #   Esperar Uma Informação Carregar Na Tela
+
+# Essa automação pode ser feita de 2 maneiras
+#   Opção 1 : Usando uma função do selenium
 """from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -34,6 +37,10 @@ driver.get('https://medium.com/dev-cave/webdriverwait-fazendo-o-selenium-esperar
 
 segundos = 50
 elemento = WebDriverWait(driver, segundos).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[3]/div[2]/div[4]/div/div/div/div[1]/div[1]/div/button')))
-
-elemento.click()"""
+elemento.click()
+"""
+#   Opção 2 : criar uma função
+"""while len(driver.find_elements(by=By.XPATH, value='//*[@id="root"]/div/div[3]/div[2]/div[4]/div/div/div/div[1]/div[1]/div/button'))==0:
+    sleep(1)
+driver.find_element(by=By.XPATH, value='//*[@id="root"]/div/div[3]/div[2]/div[4]/div/div/div/div[1]/div[1]/div/button').click()"""
 sleep(1000)
