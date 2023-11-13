d25ee90d2aa5ab685fc2972549f4962f1cc99586
ZhouZKZ <roberto16092004@gmail.com>
2023-11-13 01:20:33 -0300

    Códigos Úteis do Módulo

--- Programa/Info.py
+++ Programa/Info.py
@@ -8,12 +8,14 @@ from time import sleep
 driver.get("https://google.com")
 print(driver.title)"""
 
+
 #   Abrir o Navegador sem mostrar
 """chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://google.com")
 print(driver.title)"""
+
 
 #   Abrir um Navegador no Modo Privado (Anônima) 
 """chrome_options = webdriver.ChromeOptions()
@@ -22,15 +24,14 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://google.com")
 print(driver.title)"""
 
-#   Esperar Uma Informação Carregar Na Tela
 
-# Essa automação pode ser feita de 2 maneiras
-#   Opção 1 : Usando uma função do selenium
+#   Esperar Uma Informação Carregar Na Tela(Essa automação pode ser feita de 2 maneiras)
+# Opção 1 : Usando uma função do selenium
 """from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-chrome_options = webdriver.ChromeOptions()
 
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://medium.com/dev-cave/webdriverwait-fazendo-o-selenium-esperar-a093abeb747b')
@@ -39,8 +40,43 @@ segundos = 50
 elemento = WebDriverWait(driver, segundos).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[3]/div[2]/div[4]/div/div/div/div[1]/div[1]/div/button')))
 elemento.click()
 """
-#   Opção 2 : criar uma função
+# Opção 2 : criar um while que verifica se o elemento apareceu(se não aparecer = 0 | se aparecer = 1)
 """while len(driver.find_elements(by=By.XPATH, value='//*[@id="root"]/div/div[3]/div[2]/div[4]/div/div/div/div[1]/div[1]/div/button'))==0:
     sleep(1)
 driver.find_element(by=By.XPATH, value='//*[@id="root"]/div/div[3]/div[2]/div[4]/div/div/div/div[1]/div[1]/div/button').click()"""
+
+
+#   Dar um scroll na página(Usado para carregar informações da página não carregada)
+"""chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://www.youtube.com/')
+for i in range(5):      #   Da um scroll para baixo 5x
+    sleep(2)
+    scroll = 4000 * i
+    driver.execute_script(f"window.scroll(0, {scroll})")    # Executa um script em JavaScript"""
+
+
+#   Iframe - Páginas Dentro De Páginas
+"""from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+driver = webdriver.Chrome(options=chrome_options)
+link = "https://pbdatatrader.com.br/jogosdodia"
+driver.get(link)
+sleep(7)
+#   Entrando dentro de um iframe
+iframe = driver.find_element(by=By.TAG_NAME, value='iframe')
+driver.switch_to.frame(iframe)
+
+#   Entrando dentro de um iframe dentro de outro iframe
+iframe = driver.find_element(by=By.TAG_NAME, value='iframe')
+driver.switch_to.frame(iframe)
+
+texto = driver.find_element(by=By.XPATH, value='//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[19]/transform/div/div[3]/div/div/visual-modern/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div[4]')
+print(texto.text)
+
+#   Voltando para a página principal (Saindo do Iframe)
+driver.switch_to.default_content()"""
 sleep(1000)
